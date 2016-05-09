cd213095fcd8b5a7cac95dd32b3efaea8bdb6158
nov1n <robert@carosi.nl>
2016-05-09 12:22:41 +0200

    Add jobmanager

---
+++ jobs/jobmanager_test.go
@@ -0,0 +1,83 @@
+package workflow
+
+import (
+	"fmt"
+	"testing"
+
+	"k8s.io/kubernetes/pkg/api"
+	"k8s.io/kubernetes/pkg/apis/batch"
+)
+
+const (
+	NAMESPACE = "my-workflows"
+)
+
+func TestJobCreation(t *testing.T) {
+	// Create rest client
+	client, err := newRESTClient()
+	if err != nil {
+		t.Error("Could not create REST client.")
+	}
+
+	// Create namespace
+	namespace := &api.Namespace{
+		ObjectMeta: api.ObjectMeta{
+			Name: NAMESPACE,
+		},
+	}
+	_, err = client.Namespaces().Create(namespace)
+	if err != nil {
+		t.Error(fmt.Sprintf("Could not create namespace '%s':", NAMESPACE, err))
+	}
+
+	// Add job
+	jobManager := &JobManager{
+		Client:    client,
+		Namespace: NAMESPACE,
+	}
+
+	jobConfig := &batch.Job{
+		ObjectMeta: api.ObjectMeta{
+			Name: "my-job",
+		},
+		Spec: batch.JobSpec{
+			Template: api.PodTemplateSpec{
+				ObjectMeta: api.ObjectMeta{
+					Name: "my-job",
+				},
+				Spec: api.PodSpec{
+					Containers: []api.Container{
+						api.Container{
+							Name:  "nginx",
+							Image: "nginx",
+							Ports: []api.ContainerPort{
+								api.ContainerPort{
+									ContainerPort: 80,
+								},
+							},
+						},
+					},
+					RestartPolicy: "Never",
+				},
+			},
+		},
+	}
+
+	_, err = jobManager.AddJob(jobConfig)
+
+	if err != nil {
+		t.Error(fmt.Sprintf("Could not add job: %v", err))
+	}
+
+	// Remove job
+	err = jobManager.RemoveJob(jobConfig)
+	if err != nil {
+		t.Error(fmt.Sprintf("Could not delete job: %v", err))
+	}
+
+	// Cleanup
+	err = client.Namespaces().Delete(NAMESPACE)
+	if err != nil {
+		t.Error(fmt.Sprintf("Could not delete namespace '%s':", NAMESPACE, err))
+	}
+}
