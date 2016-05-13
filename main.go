b66fc484d1393c9657a8f91544b3add68003c6fe
nov1n <robert@carosi.nl>
2016-05-13 12:26:19 +0200

    Parse flags in main

--- main.go
+++ main.go
@@ -1,50 +1,40 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 
 	"net"
 
 	"github.com/nov1n/kubernetes-workflow/pkg/client"
-	"github.com/nov1n/kubernetes-workflow/pkg/job"
 
 	k8sApi "k8s.io/kubernetes/pkg/api"
 	k8sApiUnversioned "k8s.io/kubernetes/pkg/api/unversioned"
 	"k8s.io/kubernetes/pkg/client/restclient"
 )
 
-// host and port for kubie api
-const (
-	HOST = "localhost"
-	PORT = "8080"
-)
-
 func main() {
-	cl, err := client.NewRESTClient("127.0.0.1", "8080")
+	// Parse cmdline flags
+	host := flag.String("host", "127.0.0.1", "IP address of kubernetes API server")
+	port := flag.String("port", "8080", "Port of the kubernetes API server")
 
-	if err != nil {
-		panic(err)
-	}
+	flag.Parse()
 
-	jobManager := job.Manager{
-		Client:    cl,
-		Namespace: "my-workflows",
-	}
-
-	fmt.Print("Created namespace", jobManager.Namespace)
-
+	// Create client
 	thirdPartyClient, err := client.NewThirdParty(k8sApiUnversioned.GroupVersion{
 		Group:   "nerdalize.com",
 		Version: "v1alpha1",
 	}, restclient.Config{
-		Host: "http://" + net.JoinHostPort(HOST, PORT),
+		Host: "http://" + net.JoinHostPort(*host, *port),
 	})
 	if err != nil {
 		fmt.Println("Couldn't create 3rd-party client: ", err)
 		return
 	}
+
+	// Watch workflows
 	opts := k8sApi.ListOptions{}
-	w, err := thirdPartyClient.Workflows("default").Watch(opts)
+	w, err := thirdPartyClient.Workflows(k8sApi.NamespaceDefault).Watch(opts)
 	if err != nil {
 		fmt.Println("Couldn't watch workflows: ", err)
 		return
