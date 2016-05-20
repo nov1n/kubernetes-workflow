388c8c965a2826041d027a11fa2b3cfb17b75f61
Boris Mattijssen <borismattijssen@gmail.com>
2016-05-20 12:23:29 +0200

    Add utils to speed up testing process

--- hack/clear-wf.sh
+++ hack/clear-wf.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-~/dev/go/src/k8s.io/kubernetes/cluster/kubectl.sh delete jobs --all
 etcdctl rm /registry/ThirdPartyResourceData/nerdalize.com/workflows/default/test-workflow
-cat ~/dev/go/src/github.com/nov1n/kubernetes-workflow/examples/workflow.json | curl --data "@-" -H "Content-Type:application/json" http://localhost:8080/apis/nerdalize.com/v1alpha1/namespaces/default/workflows
+$PROJECTS/go/src/k8s.io/kubernetes/cluster/kubectl.sh delete jobs --all
+cat $PROJECTS/go/src/github.com/nov1n/kubernetes-workflow/examples/workflow.json | curl --data "@-" -H "Content-Type:application/json" http://localhost:8080/apis/nerdalize.com/v1alpha1/namespaces/default/workflows
 
-/usr/bin/watch ~/dev/go/src/k8s.io/kubernetes/cluster/kubectl.sh get jobs
+/usr/bin/watch $PROJECTS/go/src/k8s.io/kubernetes/cluster/kubectl.sh get jobs --show-labels
